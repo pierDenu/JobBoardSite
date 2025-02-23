503e36940ae2748a7881ddb91536a2ede4bb354d
pier-dune <2s.ivanets@gmail.com>
2025-02-23 16:50:44 +0200

    Initialized project and implemented authentication system

---
+++ authentication/views.py
@@ -0,0 +1,21 @@
+from django.contrib.auth import login, logout
+from django.shortcuts import render, redirect
+from .forms import LoginForm
+
+
+def login_view(request):
+    if request.method == "POST":
+        form = LoginForm(data=request.POST)
+        if form.is_valid():
+            user = form.get_user()
+            login(request, user)
+            return redirect('home')  # Redirect to home page after login
+    else:
+        form = LoginForm()
+
+    return render(request, 'accounts/login.html', {'form': form})
+
+
+def logout_view(request):
+    logout(request)
+    return redirect('login')  # Redirect to login page after logout
